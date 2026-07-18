33878723a05a6f9555ded0cbac0a9e494dc5f7d2
kc <krzysztof.ciepka@gmail.com>
2026-07-19 00:38:50 +0200

    feat: add table output formatter

--- internal/output/table.go
+++ internal/output/table.go
@@ -1,1 +1,30 @@
 package output
+
+import (
+	"os"
+
+	"github.com/krzysztofciepka/key-scanner/internal/scanner"
+	"github.com/jedib0t/go-pretty/v6/table"
+)
+
+func PrintTable(results []scanner.Result) {
+	if len(results) == 0 {
+		os.Stdout.WriteString("No exposed keys found.\n")
+		return
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"KEY", "REPO", "FILE", "COMMIT DATE", "VALUE"})
+
+	for _, r := range results {
+		value := r.Value
+		if len(value) > 30 {
+			value = value[:27] + "..."
+		}
+		t.AppendRow(table.Row{r.Key, r.Repo, r.File, r.CommitDate, value})
+	}
+
+	t.SetStyle(table.StyleLight)
+	t.Render()
+}
